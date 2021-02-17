c56adfc0e30ff76750f5ba839349ebdd418635d2
Johannes Jungkunst <macevil@gmail.com>
2021-02-17 14:57:47 +0100

    added imports

--- src/main/kotlin/de/kaffeemann/kch/GreetingController.kt
+++ src/main/kotlin/de/kaffeemann/kch/GreetingController.kt
@@ -1,5 +1,9 @@
 package de.kaffeemann.kch
 
+import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+
 @RestController
 public class GreetingController {
     var template: String = "Hello, %s!"

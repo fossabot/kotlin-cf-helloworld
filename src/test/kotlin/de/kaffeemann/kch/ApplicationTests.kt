42e61ea03e1f2286ec1c4cd67d71424c01247255
Johannes Jungkunst <macevil@gmail.com>
2021-02-17 15:11:38 +0100

    Update ApplicationTests.kt

--- src/test/kotlin/de/kaffeemann/kch/ApplicationTests.kt
+++ src/test/kotlin/de/kaffeemann/kch/ApplicationTests.kt
@@ -1,10 +1,10 @@
-package de.kaffeemann.kotlincfhelloworld
+package de.kaffeemann.kch
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class KotlinCfHelloworldApplicationTests {
+class ApplicationTests {
 
 	@Test
 	fun contextLoads() {
